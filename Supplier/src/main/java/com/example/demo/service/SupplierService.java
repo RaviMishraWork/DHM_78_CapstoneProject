f5be0929bd828a5c53f5e4163b607a03980cfba4
= <=>
2025-03-26 16:31:07 -0400

    added other functions

--- Supplier/src/main/java/com/example/demo/service/SupplierService.java
+++ Supplier/src/main/java/com/example/demo/service/SupplierService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,8 @@ import com.example.demo.dao.SupplierDao;
 import com.example.demo.entity.SupplierEntity;
 import com.example.demo.repository.SupplierRepository;
 
+import jakarta.ws.rs.sse.Sse;
+
 
 @Service
 public class SupplierService {
@@ -16,17 +19,59 @@ public class SupplierService {
 	@Autowired
 	private SupplierRepository supplierRepo;
 	
-	public SupplierEntity insertSupplier(SupplierDao supplierDao) {
-		SupplierEntity se = SupplierEntity.builder()
-				.contactInfo(supplierDao.getContactInfo())
-				.rating(supplierDao.getRating())
+	private SupplierDao convertToDao(SupplierEntity supplierEntity) {
+		return SupplierDao.builder()
+				.contactInfo(supplierEntity.getContactInfo())
+				.rating(supplierEntity.getRating())
 				.build();
-		return supplierRepo.save(se);
 	}
 	
-	public List<SupplierEntity> getAllSuppliers() {
-		List<SupplierEntity> ses = supplierRepo.findAll();
-		return ses;
+	private SupplierEntity convertToEntity(SupplierDao supploerDao) {
+		return SupplierEntity.builder()
+				.contactInfo(supploerDao.getContactInfo())
+				.rating(supploerDao.getRating())
+				.build();
+	}
+	
+	public SupplierDao insertSupplier(SupplierDao supplierDao) {
+		SupplierEntity se = convertToEntity(supplierDao);
+		supplierRepo.save(se);
+		return convertToDao(se);
+	}
+	
+	public List<SupplierDao> getAllSuppliers() {
+		return supplierRepo.findAll().stream()
+				.map((se) -> {
+					return convertToDao(se);
+				}).collect(Collectors.toList());
+	}
+	
+	public SupplierDao getSupplierById(int id) {
+		return convertToDao(supplierRepo.findById(id).orElse(null));
+	}
+
+	public SupplierDao updateSupplier(int id, SupplierDao supplierDao) {
+		SupplierEntity se = supplierRepo.findById(id).orElse(null);
+		
+		if (se != null) {
+			se = convertToEntity(supplierDao);
+			se = supplierRepo.save(se);
+			return convertToDao(se);
+		} else {
+			return null;
+		}
+	}
+	
+	public SupplierDao deleteSupplier(int id) {
+		SupplierEntity se = supplierRepo.findById(id).orElse(null);
+		
+		if (se != null) {
+			supplierRepo.delete(se);
+			return convertToDao(se);
+		} else {
+			return null;
+		}
+		
 		
 	}
 	

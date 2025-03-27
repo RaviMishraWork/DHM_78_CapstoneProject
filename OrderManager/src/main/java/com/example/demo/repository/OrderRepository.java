64a8c5e2facef2daa7cda6b2a00a9bf9e4fae559
Olive Daly <olive-branch@posteo.me>
2025-03-27 18:36:12 -0400

    add gitignores and change method to find all orders

--- OrderManager/src/main/java/com/example/demo/repository/OrderRepository.java
+++ OrderManager/src/main/java/com/example/demo/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.example.demo.repository;
 
 import com.example.demo.data.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    List<Order> getAll();
+//	    List<Order> findAll();
 }
--- OrderManager/src/main/java/com/example/demo/service/OrderService.java
+++ OrderManager/src/main/java/com/example/demo/service/OrderService.java
@@ -1,12 +1,21 @@
 package com.example.demo.service;
 
 import com.example.demo.data.Order;
+import com.example.demo.data.OrderProduct;
 import com.example.demo.dto.OrderDTO;
-import com.example.demo.dto.OrderRequest;
+import com.example.demo.dto.OrderRequestDTO;
+import com.example.demo.dto.ProductDTO;
+import com.example.demo.dto.SupplierDTO;
+import com.example.demo.repository.OrderProductRepository;
 import com.example.demo.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -14,8 +23,76 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
-//    @Transactional
-//    public OrderDTO createOrder(OrderRequest orderRequest) {
-//
-//    }
+    @Autowired
+    private OrderProductRepository orderProductRepository;
+
+    @Transactional
+    public OrderDTO createOrder(OrderRequestDTO OrderRequest) {
+        Order order = Order.builder()
+                .supplierId(OrderRequest.getSupplier_id())
+                .build();
+
+        order = orderRepository.save(order);
+
+        HashMap<Integer, Integer> productSkusAndQuantities = OrderRequest.getProductSkusAndQuantities();
+
+        Order finalOrder = order;
+        productSkusAndQuantities.forEach((sku, quantity) -> {
+            OrderProduct orderProduct = OrderProduct.builder()
+                    .orderId(finalOrder.getOrderId())
+                    .productId(sku)
+                    .productQuantity(quantity)
+                    .build();
+            orderProductRepository.save(orderProduct);
+        });
+
+        return convertOrderToOrderDTO(finalOrder);
+    }
+
+    public OrderDTO getOrderById(UUID orderId) {
+        Order order = orderRepository.getReferenceById(orderId);
+        return convertOrderToOrderDTO(order);
+    }
+
+    public List<OrderDTO> getAllOrder() {
+        List<Order> orderList = orderRepository.findAll();
+        return orderList.stream().map(this::convertOrderToOrderDTO).collect(Collectors.toList());
+    }
+
+    public OrderDTO updateOrder(UUID orderId, OrderRequestDTO OrderRequest) {
+        Order order = orderRepository.getReferenceById(orderId);
+        order = orderRepository.save(order);
+        OrderDTO orderDTO = convertOrderToOrderDTO(order);
+        List<OrderProduct> orderProducts = orderProductRepository.findAllByOrderId(order.getOrderId());
+        orderProductRepository.saveAll(orderProducts);
+        return orderDTO;
+    }
+
+    public OrderDTO deleteOrder(UUID orderId) {
+        Order order = orderRepository.getReferenceById(orderId);
+        orderRepository.delete(order);
+
+        OrderDTO orderDTO = convertOrderToOrderDTO(order);
+        List<OrderProduct> orderProducts = orderProductRepository.findAllByOrderId(order.getOrderId());
+        orderProductRepository.deleteAll(orderProducts);
+        return orderDTO;
+    }
+
+    private OrderDTO convertOrderToOrderDTO(Order finalOrder) {
+        List<OrderProduct> orderProducts = orderProductRepository.findAllByOrderId(finalOrder.getOrderId());
+
+        List<ProductDTO> productDTOList = orderProducts.stream().map(orderProduct -> ProductDTO.builder()
+                .productId(orderProduct.getProductId())
+                .productQuantity(orderProduct.getProductQuantity())
+                .build()).toList();
+
+        SupplierDTO supplierDTO = SupplierDTO.builder().id(finalOrder.getSupplierId()).build();
+
+        return OrderDTO.builder()
+                .orderId(finalOrder.getOrderId())
+                .productList(productDTOList)
+                .supplier(supplierDTO)
+                .build();
+    }
+
 }
